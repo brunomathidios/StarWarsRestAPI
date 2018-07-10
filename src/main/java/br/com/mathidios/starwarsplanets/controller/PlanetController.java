3a5e5d0c464e91a1c59bd159078789e9bc485adf
Bruno Mathidios EDS <brunomathidios@yahoo.com.br>
2018-07-10 12:20:23 -0300

    services

--- src/main/java/br/com/mathidios/starwarsplanets/controller/PlanetController.java
+++ src/main/java/br/com/mathidios/starwarsplanets/controller/PlanetController.java
@@ -1,11 +1,22 @@
 package br.com.mathidios.starwarsplanets.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.mathidios.starwarsplanets.model.Planet;
 import br.com.mathidios.starwarsplanets.service.PlanetService;
 import br.com.mathidios.starwarsplanets.swapi.PlanetSWAPI;
 import br.com.mathidios.starwarsplanets.swapi.SWModel;
@@ -17,14 +28,64 @@ import retrofit2.Response;
 @RestController
 @RequestMapping("/planets")
 public class PlanetController {
-
+	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private PlanetService planetService;
 	
-	@GetMapping("/{nmPlanet}")
-	public void getAllPlanetsPublicAPI(@PathVariable(name="nmPlanet") String nmPlanet) throws Exception {
+	@PostMapping("/create")
+	public ResponseEntity<Planet> createNewPlanet(@RequestBody Planet planet) {
+		try {
+			
+			int filmsNumber = this.getFilmsNumberFromStarWarsAPIByPlanetName(planet.getNmPlanet());
+			planet.setNumberMovies(filmsNumber);
+			return Optional
+			        .ofNullable( this.planetService.create(planet) )
+			        .map( newPlanet -> ResponseEntity.ok().body(newPlanet) ) 
+			        .orElseGet( () -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build() );
+			
+		} catch (Exception e) {
+			log.error("Error create new planet: ", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Planet>> getAllPlanets(){
+		return Optional
+				.ofNullable( this.planetService.findAll() )
+				.map( planetList -> ResponseEntity.ok().body(planetList) )
+				.orElseGet( () -> ResponseEntity.notFound().build() );
 		
-		int filmsNumber = this.getFilmsNumberFromStarWarsAPIByPlanetName(nmPlanet);
+	}
+	
+	//TODO: find by name (usar UPPER)***
+	@GetMapping("/name/{nmPlanet}")
+	public ResponseEntity<List<Planet>> getPlanetByName(@PathVariable(name="nmPlanet") String nmPlanet) {
+		return Optional
+				.ofNullable( this.planetService.findByNmPlanet(nmPlanet) )
+				.map( planetList -> ResponseEntity.ok().body(planetList) )
+				.orElseGet( () -> ResponseEntity.notFound().build() );
+	}
+	
+	@GetMapping("/{idPlanet}")
+	public ResponseEntity<Planet> getPlanetById(@PathVariable(name="idPlanet") Long idPlanet) {
+		return Optional
+		        .ofNullable( this.planetService.findByIdPlanet(idPlanet) )
+		        .map( planet -> ResponseEntity.ok().body(planet) ) 
+		        .orElseGet( () -> ResponseEntity.notFound().build() ); 
+	}
+	
+	@DeleteMapping("/delete/{idPlanet}")
+	public ResponseEntity<?> deletePlanetById(@PathVariable(name="idPlanet") Long idPlanet) {
+		try {
+			this.planetService.deleteByIdPlanet(idPlanet);	
+			return ResponseEntity.status(HttpStatus.OK).build();
+		} catch (Exception e) {
+			log.error("Error create new planet: ", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	private int getFilmsNumberFromStarWarsAPIByPlanetName(String nmPlanet) throws Exception {
@@ -36,11 +97,14 @@ public class PlanetController {
 		
 		if (response.isSuccessful()) {
             SWModel data = response.body();
-            PlanetSWAPI planet = data.getResults().get(0);
-            System.out.println("Planet: " + planet.getName() + " films: " + (planet.getFilms() != null && !planet.getFilms().isEmpty() ? planet.getFilms().size() : 0));
-        	return planet.getFilms() != null && !planet.getFilms().isEmpty() ? planet.getFilms().size() : 0;
-		} else {
-			return 0;
-		}
+            
+            if(data.getResults() != null && !data.getResults().isEmpty()) {
+            	
+            	PlanetSWAPI planet = data.getResults().get(0);
+                System.out.println("Planet: " + planet.getName() + " films: " + (planet.getFilms() != null && !planet.getFilms().isEmpty() ? planet.getFilms().size() : 0));
+            	return planet.getFilms() != null && !planet.getFilms().isEmpty() ? planet.getFilms().size() : 0;
+            } 
+		} 
+		return 0;
 	}
 }
--- src/main/java/br/com/mathidios/starwarsplanets/service/impl/PlanetServiceImpl.java
+++ src/main/java/br/com/mathidios/starwarsplanets/service/impl/PlanetServiceImpl.java
@@ -3,6 +3,8 @@ package br.com.mathidios.starwarsplanets.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,35 +15,62 @@ import br.com.mathidios.starwarsplanets.service.PlanetService;
 @Service
 public class PlanetServiceImpl implements PlanetService {
 	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private PlanetDAO planetDAO;
 
 	@Override
 	public Planet create(Planet planet) {
-		return this.planetDAO.save(planet);
+		try {
+			return this.planetDAO.save(planet);
+		} catch (Exception e) {
+			log.error("Error create: ", e);
+			return null;
+		}
 	}
 
 	@Override
 	public List<Planet> findAll() {
-		List<Planet> planetList = this.planetDAO.findAll();
-		return (planetList != null && !planetList.isEmpty()) ? planetList : null;
+		try {
+			List<Planet> planetList = this.planetDAO.findAll();
+			return (planetList != null && !planetList.isEmpty()) ? planetList : null;
+		} catch (Exception e) {
+			log.error("Error findAll: ", e);
+			return null;
+		}
 	}
 
 	@Override
 	public List<Planet> findByNmPlanet(String nmPlanet) {
-		List<Planet> planetList = this.planetDAO.findByNmPlanetLike("%" + nmPlanet + "%");
-		return (planetList != null && !planetList.isEmpty()) ? planetList : null;
+		try {
+			List<Planet> planetList = this.planetDAO.findByNmPlanetLike("%" + nmPlanet + "%");
+			return (planetList != null && !planetList.isEmpty()) ? planetList : null;
+		} catch (Exception e) {
+			log.error("Error findByNmPlanet: ", e);
+			return null;
+		}
 	}
 
 	@Override
 	public Planet findByIdPlanet(Long idPlanet) {
-		Optional<Planet> planet = this.planetDAO.findById(idPlanet);
-		return planet.orElse(null);
+		try {
+			Optional<Planet> planet = this.planetDAO.findById(idPlanet);
+			return planet.orElse(null);
+		} catch (Exception e) {
+			log.error("Error findByIdPlanet: ", e);
+			return null;
+		}
 	}
 
 	@Override
 	public void deleteByIdPlanet(Long idPlanet) {
-		this.planetDAO.deleteById(idPlanet);
+		try {
+			this.planetDAO.deleteById(idPlanet);
+		} catch (Exception e) {
+			log.error("Error deleteByIdPlanet: ", e);
+			throw new RuntimeException(e);
+		}
 		
 	}
 
